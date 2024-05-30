6a51d30e6111756a43ff914e5bb3eab3f959b453
shinjaesang <tlswltkd1011@gmail.com>
2024-05-30 10:47:24 +0900

    글 등록 처리 및 모달창

--- src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/ac/kopo/guestbook/controller/GuestBookController.java
@@ -1,13 +1,16 @@
 package kr.ac.kopo.guestbook.controller;
 
 
+import kr.ac.kopo.guestbook.dto.GuestbookDTO;
 import kr.ac.kopo.guestbook.dto.PageRequestDTO;
 import kr.ac.kopo.guestbook.service.GuestbookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -25,5 +28,18 @@ public class GuestBookController {
         //list.html(View계층)에 방명록 목록과 화면에 보여질 때 필요한 페이지 번호들 등의 정보가 저장
         model.addAttribute("result", service.getList(pageRequestDTO));
 
+    }
+
+    @GetMapping("/register")
+    public void register(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        //새호 추가된 엔티티의 번호
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
